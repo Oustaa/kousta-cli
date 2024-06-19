cb1a944a500aa4fc502be448b0481cce3e8b5448
Oustaa <otailaba98@gmail.com>
2024-06-19 13:48:56 +0100

    updating the dev script to use nodemon and removing some deprecated dependencies, as well as fixing an error in the error.middleware

--- project/src/middleware/error.middleware.ts
+++ project/src/middleware/error.middleware.ts
@@ -1,11 +1,12 @@
-import { Request, Response } from 'express';
-import HttpException from '../utils/exceptions/http.exception';
-import ResponseHandler from '../utils/ResponseHandler';
+import { NextFunction, Request, Response } from "express";
+import HttpException from "../utils/exceptions/http.exception";
+import ResponseHandler from "../utils/ResponseHandler";
 
 function ErrorMiddleware(
   error: HttpException,
   req: Request,
   res: Response,
+  next: NextFunction,
 ): void {
   const status = error.status || 500;
 
