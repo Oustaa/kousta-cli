3cfc306f771c99cd86ee889a97238cb9c9602c55
Oustaa <otailaba98@gmail.com>
2024-06-20 14:10:16 +0100

    start pobulating env and env validation function

---
+++ utils/populateenv.js
@@ -0,0 +1,69 @@
+import fs from "node:fs";
+import path from "node:path";
+import { randomBytes } from "crypto";
+import { __dirname } from "../constants/index.js";
+import { createSpinner } from "nanospinner";
+
+export async function populateEnv({ dbType, projectName }) {
+  const spinner = createSpinner("Populatig .env file...").start();
+
+  try {
+    const envPath = path.join(process.cwd(), projectName, ".env");
+    const dbEnvDist = fs
+      .readFileSync(
+        path.join(__dirname(import.meta.url), "..", "assets", dbType, "env.txt")
+      )
+      .toString();
+
+    const envContents = fs.readFileSync(envPath).toString();
+
+    const updatedEnvContents = envContents
+      .replace("<% database %>", dbEnvDist)
+      .replace("<% access_token %>", randomBytes(64).toString("hex"))
+      .replace("<% refresh_token %>", randomBytes(64).toString("hex"));
+
+    fs.writeFileSync(envPath, updatedEnvContents);
+
+    envValidate({ dbType, projectName });
+
+    spinner.success();
+  } catch (error) {
+    console.log(error);
+    spinner.error();
+  }
+}
+
+function envValidate({ dbType, projectName }) {
+  try {
+    const envValidationPath = path.join(
+      process.cwd(),
+      projectName,
+      "src",
+      "utils",
+      "validateEnv.ts"
+    );
+    const dbEnvValidationDist = fs
+      .readFileSync(
+        path.join(
+          __dirname(import.meta.url),
+          "..",
+          "assets",
+          dbType,
+          "envValidation.txt"
+        )
+      )
+      .toString();
+
+    const envValidationContents = fs.readFileSync(envValidationPath).toString();
+
+    const updatedEnvValidationContents = envValidationContents.replace(
+      "<% env-validation %>,",
+      dbEnvValidationDist
+    );
+
+    fs.writeFileSync(envValidationPath, updatedEnvValidationContents);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
