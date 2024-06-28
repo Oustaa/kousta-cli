fe961e39dc0fad3f351a5b1a3e26819af96152e3
Oustaa <otailaba98@gmail.com>
2024-06-28 20:45:17 +0100

    adding some more make commands

--- utils/make:/index.js
+++ utils/make:/index.js
@@ -1,7 +1,11 @@
+import { askQuestion } from "../ask.js";
 import { createController } from "./createController.js";
+import { createModel } from "./createModel.js";
+import { createResource } from "./createResource.js";
+import { createRequest } from "./createRequest.js";
 import { createHttp } from "./createHttp.js";
 
-async function makeResource(name) {
+async function makeHttp(name) {
   const resourceName = name
     ? name
     : await askQuestion({
@@ -18,12 +22,14 @@ async function makeResource(name) {
 
 export function initMakeCommands(program, name) {
   program
-    .command("make:resource [resource-name]")
-    .description("Create a new resource with specified name")
-    .action(makeResource)
+    .command("make:http-group [resource-name]")
+    .description(
+      "Create a new http group ( C, M, RES, REQ ) with specified name"
+    )
+    .action(makeHttp)
     .on("--help", () => {
       console.log("\nExamples:");
-      console.log(`  $ ${name} make:resource`);
+      console.log(`  $ ${name} make:http-group`);
     });
 
   program
@@ -33,6 +39,33 @@ export function initMakeCommands(program, name) {
     .on("--help", () => {
       console.log("\nExamples:");
       console.log(`  $ ${name} make:controller`);
+    });
+
+  program
+    .command("make:model [model-name]")
+    .description("Create a new model with specified name")
+    .action(createModel)
+    .on("--help", () => {
+      console.log("\nExamples:");
+      console.log(`  $ ${name} make:model`);
+    });
+
+  program
+    .command("make:resource [resource-name]")
+    .description("Create a new resource with specified name")
+    .action(createResource)
+    .on("--help", () => {
+      console.log("\nExamples:");
+      console.log(`  $ ${name} make:resource`);
+    });
+
+  program
+    .command("make:request [request-name]")
+    .description("Create a new request with specified name")
+    .action(createRequest)
+    .on("--help", () => {
+      console.log("\nExamples:");
+      console.log(`  $ ${name} make:request`);
     });
 }
 
