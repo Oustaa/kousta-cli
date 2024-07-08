b686727cc1ae692ae8b16a0c45a50d3c7af7333b
Oustaa <otailaba98@gmail.com>
2024-07-08 20:50:05 +0100

    completing the staps for the auth file

--- project/src/utils/ResponseHandler.ts
+++ project/src/utils/ResponseHandler.ts
@@ -1,6 +1,6 @@
 import { Response } from 'express';
 import logger from './Logger';
-import { PaginationMetaData } from "../kousta/@utils/getpagination";
+import { PaginationMetaData } from "../kousta/@helpers";
 
 export default class ResponseHandler {
   private static logger = logger;
