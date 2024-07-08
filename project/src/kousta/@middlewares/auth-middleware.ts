f064a121750ffb5621de3e87260d21949d2eb245
Oustaa <otailaba98@gmail.com>
2024-07-08 20:52:42 +0100

    adding a middleware to handel auth requests, its a decorators base middleware

--- project/src/kousta/@middlewares/auth-middleware.ts
+++ project/src/kousta/@middlewares/auth-middleware.ts
@@ -1,8 +1,41 @@
-export function authorizeAndLog(originalMethod: any, _context: any) {
-  return function (this: any, ...args: any[]) {
-    console.log("start:", originalMethod.name);
-    const result = originalMethod.call(this, ...args);
-    console.log("end:", originalMethod.name);
-    return result;
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import HttpException from "@/Utils/exceptions/http.exception";
+
+export function isAuthorised(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor,
+) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = async function (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      let token = req.headers["authorization"];
+
+      if (token) {
+        token = token.substring(7);
+        const user = jwt.verify(token, process.env.ACCESS_TOKEN!);
+
+        if (user) {
+          req.user = user;
+        } else {
+          throw new HttpException(403, "Invalid token");
+        }
+      } else {
+        throw new HttpException(403, "No token provided");
+      }
+
+      const result = await originalMethod.call(this, req, res, next);
+      return result;
+    } catch (error) {
+      next(error);
+    }
   };
+
+  return descriptor;
 }
